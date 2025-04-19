bdd58c34fbdba75a35d89596a6ebb8c59733ada2
saimucherla9 <109085947+saimucherla9@users.noreply.github.com>
2025-04-19 19:54:40 -0400

    main executable

---
+++ src/main/java/com/sai/demo/StringConverterApplication.java
@@ -0,0 +1,12 @@
+package com.sai.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringConverterApplication.class, args);
+	}
+}
